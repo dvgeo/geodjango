04772bea0e69a20b38dd438059982e1ab94c13eb
Your Name <dvgeo>
2020-05-04 14:16:30 -0300

    Update views.py

--- agricom/reporter/views.py
+++ agricom/reporter/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from models import County,Incidence
+
+
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
