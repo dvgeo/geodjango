efffdf1a3248f33ccc6406ab1ba741df8af18c6b
Your Name <dvgeo>
2020-05-05 07:52:22 -0300

    Update urls.py

--- agricom/reporter/urls.py
+++ agricom/reporter/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', HomePageView.as_view(), name= 'home'),
+    path('incidences/', IncidenceData, name='incidences'),
+    path('county/', CountyData, name='counties')
 ]
